0030be9cdc18de677688553859e49d9efa7cff3d
Amit <support@ellaisys.com>
2022-06-23 19:07:41 +0530

    doc: updated the exception document

--- src/Exceptions/ExotelException.php
+++ src/Exceptions/ExotelException.php
@@ -9,6 +9,21 @@ class ExotelException extends Exception
 {
 
     /**
+     * HTTP Status Codes: Exotel uses standard HTTP status codes to communicate errors
+     * 
+     * Status Code: Value
+     * 200: OK - Everything went as planned.
+     * 202: Accepted - Request accepted.
+     * 400: Bad Request - Something in your header or request body was malformed.
+     * 401: Unauthorised - Necessary credentials were either missing or invalid.
+     * 402: Payment Required - The action is not available on your plan, or you have exceeded usage limits for your current plan.
+     * 403: Your credentials are valid, but you don’t have access to the requested resource.
+     * 404: Not Found - The object you’re requesting doesn’t exist.
+     * 409: Conflict - You might be trying to update the same resource concurrently.
+     * 429: Too Many Requests - You are calling our APIs more frequently than we allow.
+     */
+
+    /**
      * Create exotel exception instance.
      *
      * @param string     $message
